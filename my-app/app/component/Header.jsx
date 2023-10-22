bcc27347bdda311f45cde6ec80fdb0fafa466079
Rajdeep De <surjode2003@gmail.com>
2023-10-22 11:35:32 +0530

    set up logo

---
+++ my-app/app/component/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+function Header() {
+  return (
+    <div>
+      <div>
+        <img src="/logo-volvo.png" alt="image" />
+      </div>
+    </div>
+  );
+}
+
+export default Header
