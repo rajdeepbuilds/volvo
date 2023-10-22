be9b7e2de336ade1d6c17c7d0630397d7d85d4b6
Rajdeep De <surjode2003@gmail.com>
2023-10-23 00:49:12 +0530

    setting up the vid part

--- my-app/app/component/VideoPart.jsx
+++ my-app/app/component/VideoPart.jsx
@@ -1,9 +1,35 @@
 import React from 'react'
 
+
 function VideoPart() {
   return (
-    <div>VideoPart</div>
-  )
+    <div>
+      <video autoPlay loop muted playsInline className="absolute z[-1]">
+        <source src="/volvo-video.mp4" />
+      </video>
+    </div>
+  );
 }
 
 export default VideoPart
+
+
+
+
+/*<div className="w-full h-screen">
+      <div>
+        <video
+          autoPlay
+          loop
+          muted
+          className=" w-full h-full object-cover"
+        >
+          <source src="/volvo-video.mp4" />
+        </video>
+        <div className="flex flex-col  absolute w-full h-full top-[50%] -translate-x-2/4 -translate-y-2/4 justify-center items-center text-white">
+          <h2>A new era for safety</h2>
+          <h1>Does How you feel affect</h1>
+          <h1>How you drive</h1>
+        </div>
+      </div>
+    </div>*/
